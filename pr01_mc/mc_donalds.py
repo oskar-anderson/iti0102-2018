d51a0789ead0fded5ef5425d44c8fec6683a7495
kaande <kaande@ttu.ee>
2018-09-06 17:03:26 +0300

    Esimene koodimine- Docstring on tüütu.

---
+++ pr01_mc/mc_donalds.py
@@ -0,0 +1,11 @@
+""""McDonald's Dialogue."""
+print("Welcome to McDonald's!")
+drink = input("What would you like to drink?")
+print(drink + " with ice cubes is my favorite too!")
+food = input("Can I take your order?")
+if food == "Big Mac":
+    print("Big Mac meal coming right up!")
+elif food == "Salad":
+    print("Hmm, that's weird.")
+else:
+    print("We only serve junk food.")
