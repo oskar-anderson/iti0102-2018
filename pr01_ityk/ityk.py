909c2c791bfbcc60f1f8789a4e228a90e4c12788
kaande <kaande@ttu.ee>
2018-09-06 17:25:42 +0300

    Esimene koodimine...v1

---
+++ pr01_ityk/ityk.py
@@ -0,0 +1,11 @@
+"""Would you like to join itük?"""
+print("Welcome to itük!")
+person_name = input("what is your name?")
+print("Hi there, " + person_name + "!")
+decision = input("Would you like to join itük?")
+if decision == "yes":
+    print("Welcome to the club!")
+elif decision == "no":
+    print("Why tho?")
+else:
+    print("Ok, see you next year!")
