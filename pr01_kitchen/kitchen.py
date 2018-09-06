a40b51abcba50d474b0572d8a3bde32236edb45e
kaande <kaande@ttu.ee>
2018-09-06 14:02:40 +0300

    Esimene koodimine...v1

--- pr01_kitchen/kitchen.py
+++ pr01_kitchen/kitchen.py
@@ -1,10 +1,10 @@
 print("Welcome to the kitchen.")
 name = input("What is your name?")
 print("Hi there, " + name + "!")
-jook = input("What would you like to drink?")
-if jook == "tea":
+drink = input("What would you like to drink?")
+if drink == "tea":
     print("Have a nice tea!")
-elif jook == "coffee":
+elif drink == "coffee":
     print("Feeling tired?")
 else:
     print("We only serve water.")
