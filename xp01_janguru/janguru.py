a7f7df5e03f681aaf272868efb51770e9aceafaa
kaande <kaande@ttu.ee>
2018-09-11 11:18:22 +0300

    Jänguru

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -6,6 +6,8 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
     max_sleep1 = sleep1
     max_sleep2 = sleep2
     run_time = 100000
+    starting_pos1 = pos1
+    starting_pos2 = pos2
     # the higher the better, but takes more time
 
     pos1 += jump_distance1
@@ -14,6 +16,9 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
 
     while pos1 != pos2 and run_time > 0:
         run_time = run_time - 1
+        if run_time == 1 and abs(pos1-pos2) < abs(starting_pos1-starting_pos2):
+            run_time = 1000000
+            # resets run_time if the rabbits have gotten closure since the start
 
         # Jumping
         if sleep1 == 0:
@@ -47,4 +52,9 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
 
 # meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2)
 
-# meet_me(100, 7, 4, 300, 8, 6)
+meet_me(1, 2, 1, 2, 1, 1)
+meet_me(1, 2, 3, 4, 5, 5)
+meet_me(10, 7, 7, 5, 8, 6)
+meet_me(100, 7, 4, 300, 8, 6)
+meet_me(1, 7, 1, 15, 5, 1)
+meet_me(0, 1, 1, 1, 1, 1)
