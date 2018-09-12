348e39d9e9c9a2c527c1ad556ba251acce911777
kaande <kaande@ttu.ee>
2018-09-12 15:11:01 +0300

    Points relation to a circle.

--- pr02_circle/pr02_circle.py
+++ pr02_circle/pr02_circle.py
@@ -4,8 +4,11 @@ import math
 
 def find_circle_info(d, x, y):
     """
-    The function should print "Circle with perimeter of {perimeter} units and area of {area} units has point ({x}, {y})\
-    on its {place}".
+    Finds perimeter, area and where given point is placed in relation to the circle with diameter d.
+
+    Place: inside, perimeter, outside.
+    The function should print "Circle with perimeter of {perimeter} units and area of {area} units has point
+    ({x}, {y}) on its {place}".
     :return: None
     """
     perimeter = d * math.pi
