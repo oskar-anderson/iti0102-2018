695350ab0ffa0f24ef6c7b129a27d820e3a78257
kaande <kaande@ttu.ee>
2018-09-12 17:45:53 +0300

    Jänguru- now with quicker sleep

--- xp01_janguru/janguru.py
+++ xp01_janguru/janguru.py
@@ -16,6 +16,9 @@ def meet_me(pos1, jump_distance1, sleep1, pos2, jump_distance2, sleep2):
 
     while pos1 != pos2 and run_time > 0:
         run_time = run_time - 1
+        if run_time == 1 and abs(pos1 - pos2) < abs(starting_pos1 - starting_pos2):
+            run_time = 1000000
+            # resets run_time if the rabbits have gotten closure since the start
 
         # Jumping
         if sleep1 == 0:
