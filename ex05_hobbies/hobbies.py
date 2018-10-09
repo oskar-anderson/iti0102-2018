ca115783f2e764374c9785f56a306994fba10291
kaande <kaande@ttu.ee>
2018-10-09 23:51:49 +0300

    hobbies

--- ex05_hobbies/hobbies.py
+++ ex05_hobbies/hobbies.py
@@ -20,7 +20,6 @@ def create_dictionary(file):
     :param file: original file path
     :return: dict
     """
-    #
     dict1 = {}
     for i in create_list_from_file(file):
         n = i.find(":")
