f05cb6193ce1be15cfb1b5189a69a32b2d08bd89
kaande <kaande@ttu.ee>
2018-10-10 22:41:17 +0300

    hobbies

--- ex05_hobbies/hobbies.py
+++ ex05_hobbies/hobbies.py
@@ -139,21 +139,21 @@ def write_corrected_database(file, file_to_write):
 if __name__ == '__main__':
     dic = create_dictionary("hobbies_database.txt")
     print(len(create_list_from_file("hobbies_database.txt")))  # -> 100
-    print("Check presence of hobbies for chosen person:")
-    print("shopping" in dic["Wendy"])  # -> True
-    print("fitness" in dic["Sophie"])  # -> False
-    print("gaming" in dic["Peter"])  # -> True
-    print("Check if hobbies - person relation is correct:")
-    print("Check if a person(people) with the biggest amount of hobbies is(are) correct:")
-    print(find_person_with_most_hobbies("hobbies_database.txt"))  # -> ['Jack']
-    print(len(dic["Jack"]))  # ->  12
-    print(len(dic["Carmen"]))  # -> 10
-    print("Check if a person(people) with the smallest amount of hobbies is(are) correct:")
-    print(find_person_with_least_hobbies("hobbies_database.txt"))  # -> ['Molly']
-    print(len(dic["Molly"]))  # -> 5
-    print(len(dic["Sophie"]))  # -> 7
-    print("Check if the most popular hobby(ies) is(are) correct")
-    print(find_most_popular_hobby("hobbies_database.txt"))  # -> ['gaming', 'sport', 'football']
-    print("Check if the least popular hobby(ies) is(are) correct")
-    print(find_least_popular_hobby("hobbies_database.txt"))  # -> ['tennis', 'dance', 'puzzles', 'flowers']
-    write_corrected_database("hobbies_database.txt", 'correct_hobbies_database.csv')
+#    print("Check presence of hobbies for chosen person:")
+#    print("shopping" in dic["Wendy"])  # -> True
+#    print("fitness" in dic["Sophie"])  # -> False
+#    print("gaming" in dic["Peter"])  # -> True
+#    print("Check if hobbies - person relation is correct:")
+#    print("Check if a person(people) with the biggest amount of hobbies is(are) correct:")
+#    print(find_person_with_most_hobbies("hobbies_database.txt"))  # -> ['Jack']
+#    print(len(dic["Jack"]))  # ->  12
+#    print(len(dic["Carmen"]))  # -> 10
+#    print("Check if a person(people) with the smallest amount of hobbies is(are) correct:")
+#    print(find_person_with_least_hobbies("hobbies_database.txt"))  # -> ['Molly']
+#    print(len(dic["Molly"]))  # -> 5
+#    print(len(dic["Sophie"]))  # -> 7
+#    print("Check if the most popular hobby(ies) is(are) correct")
+#    print(find_most_popular_hobby("hobbies_database.txt"))  # -> ['gaming', 'sport', 'football']
+#    print("Check if the least popular hobby(ies) is(are) correct")
+#    print(find_least_popular_hobby("hobbies_database.txt"))  # -> ['tennis', 'dance', 'puzzles', 'flowers']
+#    write_corrected_database("hobbies_database.txt", 'correct_hobbies_database.csv')
