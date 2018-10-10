c97c4de66663fdc0cef5a05d6a09e4274a601477
kaande <kaande@ttu.ee>
2018-10-10 11:42:00 +0300

    schedule

---
+++ ex06_schedule/schedule.py
@@ -0,0 +1,16 @@
+"""Create schedule from the given file."""
+
+
+def create_schedule_file(input_filename: str, output_filename: str) -> None:
+    """Create schedule file from the given input file."""
+    pass
+
+
+def create_schedule_string(input_string: str) -> str:
+    """Create schedule string from the given input string."""
+    pass
+
+
+if __name__ == '__main__':
+    print(create_schedule_string("11:00 teine tekst 11:0 jah ei 10:00 pikktekst "))
+    create_schedule_file("schedule_input.txt", "schedule_output.txt")
