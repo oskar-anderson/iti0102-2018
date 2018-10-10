a3160e74de4713200e501ef1427912d9ab1eb544
kaande <kaande@ttu.ee>
2018-10-10 23:59:34 +0300

    hobbies

--- ex05_hobbies/hobbies.py
+++ ex05_hobbies/hobbies.py
@@ -1,20 +1,9 @@
 """Hobbies."""
 
 
-def create_list_from_file(file):
-    """
-    Collect lines from given file into list.
-
-    :param file: original file path
-    :return: list of lines
-    """
-    file = open(file, "r")
-    return file.readlines()
-
-
 if __name__ == '__main__':
-    dic = create_dictionary("hobbies_database.txt")
     print(len(create_list_from_file("hobbies_database.txt")))  # -> 100
+    dic = create_dictionary("hobbies_database.txt")
     print("Check presence of hobbies for chosen person:")
     print("shopping" in dic["Wendy"])  # -> True
     print("fitness" in dic["Sophie"])  # -> False
@@ -22,12 +11,12 @@ if __name__ == '__main__':
     print("Check if hobbies - person relation is correct:")
     print("Check if a person(people) with the biggest amount of hobbies is(are) correct:")
     print(find_person_with_most_hobbies("hobbies_database.txt"))  # -> ['Jack']
-    print(len(dic["Jack"])) # ->  12
-    print(len(dic["Carmen"])) # -> 10
+    print(len(dic["Jack"]))  # ->  12
+    print(len(dic["Carmen"]))  # -> 10
     print("Check if a person(people) with the smallest amount of hobbies is(are) correct:")
     print(find_person_with_least_hobbies("hobbies_database.txt"))  # -> ['Molly']
-    print(len(dic["Molly"])) # -> 5
-    print(len(dic["Sophie"])) # -> 7
+    print(len(dic["Molly"]))  # -> 5
+    print(len(dic["Sophie"]))  # -> 7
     print("Check if the most popular hobby(ies) is(are) correct")
     print(find_most_popular_hobby("hobbies_database.txt"))  # -> ['gaming', 'sport', 'football']
     print("Check if the least popular hobby(ies) is(are) correct")
