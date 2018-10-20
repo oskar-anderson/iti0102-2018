6644d78932746e6d6a2214bf3995851fbcf5a41a
kaande <kaande@ttu.ee>
2018-10-20 03:39:33 +0300

    hobbies

--- ex06_schedule/schedule.py
+++ ex06_schedule/schedule.py
@@ -12,16 +12,18 @@ def create_schedule_file(input_filename: str, output_filename: str) -> None:
 
 def create_schedule_string(input_string: str) -> str:
     """Create schedule string from the given input string."""
-    regex = r"(\d+)([\D])(\d+)[ \n]* ([a-zA-Z]*)"
+    regex = r"\s(\d+)[\D](\d+)\s+([a-zA-Z]+)"
     dict1 = {}
     for match in re.finditer(regex, input_string):
-        if int(match.group(1)) in range(24) and len(match.group(1)) in [1, 2] and int(match.group(3)) in range(60) \
-                and len(match.group(3)) in [1, 2]:
-            if int(match.group(1)) < 12 or int(match.group(1)) == 12 and int(match.group(3)) == 0:
-                time = match.group(1).zfill(2) + ":" + match.group(3).zfill(2) + " AM"
+        if int(match.group(1)) in range(24) and len(match.group(1)) in [1, 2] and int(match.group(2)) in range(60) \
+                and len(match.group(2)) in [1, 2]:
+            if int(match.group(1)) == 0:
+                time = str(int(match.group(1)) + 12).zfill(2) + ":" + match.group(2).zfill(2) + " AM"
+            elif int(match.group(1)) < 12 or int(match.group(1)) == 12 and int(match.group(2)) == 0:
+                time = match.group(1).zfill(2) + ":" + match.group(2).zfill(2) + " AM"
             else:
-                time = str(int(match.group(1)) - 12).zfill(2) + ":" + match.group(3).zfill(2) + " PM"
-            string = match.group(4).lower()
+                time = str(int(match.group(1)) - 12).zfill(2) + ":" + match.group(2).zfill(2) + " PM"
+            string = match.group(3).lower()
             dict1.setdefault(time, [])
             if string not in dict1[time]:
                 dict1.setdefault(time, []).append(string)
@@ -50,14 +52,18 @@ def create_main_content_of_table(value_max_lenght, sorted_time_list, dict1):
 
 def sort_time_list(dict1):
     """Create sorted time list from given dictionary."""
+    twelve_am = []
     time_am = []
     time_pm = []
+    print(sorted(dict1))
     for time in sorted(dict1):
-        if "AM" in time:
-                time_am.append(time)
+        if "12:" in time:
+            twelve_am.append(time)
+        elif "AM" in time:
+            time_am.append(time)
         elif "PM" in time:
-                time_pm.append(time)
-    list_of_sorted_times = time_am + time_pm
+            time_pm.append(time)
+    list_of_sorted_times = twelve_am + time_am + time_pm
     return list_of_sorted_times
 
 
