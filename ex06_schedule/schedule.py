cb7c286bbdb37c1c37ccd4235f107408fd168453
kaande <kaande@ttu.ee>
2018-10-23 20:48:12 +0300

    hobbies

--- ex06_schedule/schedule.py
+++ ex06_schedule/schedule.py
@@ -8,6 +8,7 @@ def create_schedule_file(input_filename: str, output_filename: str) -> None:
     output_file = open(output_filename, "w")
     output_file.write(create_schedule_string(input_data))
     output_file.close()
+# test1
 
 
 def create_schedule_string(input_string: str) -> str:
