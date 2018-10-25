74355dd2d4543bf2340f24d04302dc1cf5fbf3ac
kaande <kaande@ttu.ee>
2018-10-25 19:50:28 +0300

    hobbies

--- ex06_schedule/schedule.py
+++ ex06_schedule/schedule.py
@@ -8,126 +8,92 @@ def create_schedule_file(input_filename: str, output_filename: str) -> None:
     output_file = open(output_filename, "w")
     output_file.write(create_schedule_string(input_data))
     output_file.close()
-# test10
 
 
 def create_schedule_string(input_string: str) -> str:
-    """Create schedule string_lower from the given input string_lower."""
+    """Create time string pair dictionary and schedule string from the given input lowerized string."""
     regex = r"\s+([\d]{1,2})[\D]([\d]{1,2})\s+([a-zA-Z]+)"
-    dict_time_and_str_w_zfill = {}
+    dict_time_and_str = {}
     for match in re.finditer(regex, input_string):
         if int(match.group(1)) in range(24) and int(match.group(2)) in range(60):
             if int(match.group(1)) == 0:
                 time = str(int(match.group(1)) + 12) + ":" + match.group(2).zfill(2) + " AM"
+            elif int(match.group(1)) == 12:
+                time = str(int(match.group(1))) + ":" + match.group(2).zfill(2) + " PM"
             elif int(match.group(1)) < 12:
-                time = match.group(1).zfill(2) + ":" + match.group(2).zfill(2) + " AM"
+                if (match.group(1)[0]) == "0":
+                    remove_first_digit_zero = []
+                    list_hour_digits = list(match.group(1))
+                    list_hour_digits[0] = ""
+                    remove_first_digit_zero.append("".join(list_hour_digits))
+                    time = str(remove_first_digit_zero).strip("[]'") + ":" + match.group(2).zfill(2) + " AM"
+                else:
+                    time = match.group(1) + ":" + match.group(2).zfill(2) + " AM"
             else:
-                time = str(int(match.group(1)) - 12).zfill(2) + ":" + match.group(2).zfill(2) + " PM"
+                if (match.group(1)[0]) == "0":
+                    remove_first_digit_zero = []
+                    list_hour_digits = list(match.group(1))
+                    list_hour_digits[0] = ""
+                    remove_first_digit_zero.append("".join(list_hour_digits))
+                    time = str(remove_first_digit_zero).strip("[]'") + ":" + match.group(2).zfill(2) + " PM"
+                else:
+                    time = str(int(match.group(1)) - 12) + ":" + match.group(2).zfill(2) + " PM"
             string_lower = match.group(3).lower()
-            dict_time_and_str_w_zfill.setdefault(time, [])
-            if string_lower not in dict_time_and_str_w_zfill[time]:
-                dict_time_and_str_w_zfill.setdefault(time, []).append(string_lower)
-    if dict_time_and_str_w_zfill == {}:
+            dict_time_and_str.setdefault(time, [])
+            if string_lower not in dict_time_and_str[time]:
+                dict_time_and_str.setdefault(time, []).append(string_lower)
+    if dict_time_and_str == {}:
         return no_items_found()
 
-    dict_time_and_str_wo_zfill = get_dict_time_and_str_wo_zfill(input_string)
-    print(dict_time_and_str_wo_zfill)
-    proper_value_list = create_value_list(dict_time_and_str_w_zfill)
-    sorted_time_list = sort_time_list(dict_time_and_str_w_zfill)
+    proper_value_list = create_value_list(dict_time_and_str)
+    sorted_time_list = sort_time_list(dict_time_and_str)
     # print("sorted_time_list: " + str(sorted_time_list))
     max_time_lenght = find_max_time_lenght(sorted_time_list)
-    print(f"max_time_lenght: {max_time_lenght}")
     max_value_lenght = find_max_lenght(proper_value_list)
     new_line = "\n"
     separator = (max_time_lenght + max_value_lenght + 3) * "-"
     title_line = f"|" + ((max_time_lenght - 5) * " ") + "time | items" + ((max_value_lenght - 6) * " ") + "|"
     main_content = create_main_schedule_content(max_time_lenght, max_value_lenght, sorted_time_list,
-                                                dict_time_and_str_wo_zfill)
+                                                dict_time_and_str)
     result = f"{separator}\n{title_line}\n{separator}\n{new_line.join(main_content)}\n{separator}"
     print(result)
     # print("result type: " + str(type(result)))
     return result
 
 
-def get_dict_time_and_str_wo_zfill(input_string):
-    """Make a new dict with no zfill for hours."""
-    dict_time_and_str_wo_zfill = {}
-    regex = r"\s+([\d]{1,2})[\D]([\d]{1,2})\s+([a-zA-Z]+)"
-    for match in re.finditer(regex, input_string):
-        if int(match.group(1)) in range(24) and int(match.group(2)) in range(60):
-            if int(match.group(1)) == 0:
-                time = str(int(match.group(1)) + 12) + ":" + match.group(2).zfill(2) + " AM"
-            elif int(match.group(1)) < 12:
-                if (match.group(1)[0]) == "0":
-                    # print("match.group with zero: " + str(match.group(1)))
-                    remove_first_digit_zero = []
-                    list_hour_digits = list(match.group(1))
-                    list_hour_digits[0] = ""
-                    remove_first_digit_zero.append("".join(list_hour_digits))
-                    # print("remove_first_digit_zero: " + str(remove_first_digit_zero))
-                    time = str(remove_first_digit_zero).strip("[]'") + ":" + match.group(2).zfill(2) + " AM"
-                    # print(time)
-                else:
-                    time = match.group(1) + ":" + match.group(2).zfill(2) + " AM"
-            else:
-                if (match.group(1)[0]) == "0":
-                    # print("match.group with zero: " + str(match.group(1)))
-                    remove_first_digit_zero = []
-                    list_hour_digits = list(match.group(1))
-                    list_hour_digits[0] = ""
-                    remove_first_digit_zero.append("".join(list_hour_digits))
-                    # print("remove_first_digit_zero: " + str(remove_first_digit_zero))
-                    time = str(remove_first_digit_zero).strip("[]'") + ":" + match.group(2).zfill(2) + " PM"
-                    # print(time)
-                else:
-                    time = str(int(match.group(1)) - 12) + ":" + match.group(2).zfill(2) + " PM"
-            string_lower = match.group(3).lower()
-            dict_time_and_str_wo_zfill.setdefault(time, [])
-            if string_lower not in dict_time_and_str_wo_zfill[time]:
-                dict_time_and_str_wo_zfill.setdefault(time, []).append(string_lower)
-    return dict_time_and_str_wo_zfill
-
-
-def sort_time_list(dict_time_and_name_w_zfill):
+def sort_time_list(dict_time_and_name):
     """Create sorted time list from given dictionary."""
     twelve_am = []
-    twelve_pm = []
     time_am = []
+    ten_and_eleven_am = []
+    twelve_pm = []
     time_pm = []
-    print(f"Sorted dict: {sorted(dict_time_and_name_w_zfill)}")
-    for time in sorted(dict_time_and_name_w_zfill):
-        if "12:" in time and "PM" in time:
-            twelve_pm.append(time)
-        elif "12:" in time and "AM" in time:
+    ten_and_eleven_pm = []
+    # print(f"Sorted dict: {sorted(dict_time_and_name)}")
+    for time in sorted(dict_time_and_name):
+        if "12:" in time and "AM" in time:
             twelve_am.append(time)
+        elif "10:" in time and "AM" in time or "11:" in time and "AM" in time:
+            ten_and_eleven_am.append(time)
         elif "AM" in time:
             time_am.append(time)
+        elif "12:" in time and "PM" in time:
+            twelve_pm.append(time)
+        elif "10:" in time and "PM" in time or "11:" in time and "PM" in time:
+            ten_and_eleven_pm.append(time)
         elif "PM" in time:
             time_pm.append(time)
-    list_of_sorted_times = twelve_am + time_am + twelve_pm + time_pm
-    # print("list_of_sorted_times: " + str(list_of_sorted_times))
-    remove_first_digit_zero = []
-    for i in range(len(list_of_sorted_times)):
-        # print((list_of_sorted_times[i])[0])
-        if (list_of_sorted_times[i])[0] == "0":
-            listify_time = list(list_of_sorted_times[i])
-            listify_time[0] = ""
-            # print("listify_time: " + str(listify_time))
-            remove_first_digit_zero.append("".join(listify_time))
-            # print("remove_first_digit_zero: " + str(remove_first_digit_zero))
-        else:
-            remove_first_digit_zero.append(list_of_sorted_times[i])
-    # print(list_of_sorted_times)
-    # print(remove_first_digit_zero)
-    return remove_first_digit_zero
+    list_of_sorted_times = twelve_am + time_am + ten_and_eleven_am + twelve_pm + time_pm + ten_and_eleven_pm
+    return list_of_sorted_times
 
 
-def create_value_list(dict_time_and_name_w_zfill):
+def create_value_list(dict_time_and_name):
     """Put dictionary values into proper format for schedule."""
-    old_value_list = list(dict_time_and_name_w_zfill.values())
+    old_value_list = list(dict_time_and_name.values())
     value_list = []
     for i in range(len(old_value_list)):
         value_list.append(", ".join(old_value_list[i]))
+    print(f"Value list: {value_list}")
     return value_list
 
 
@@ -166,12 +132,12 @@ def no_items_found():
     return f"{separator_line}\n{title_line}\n{separator_line}\n{main_content}\n{separator_line}"
 
 
-def create_main_schedule_content(max_time_lenght, max_str_lenght, sorted_time_list, dict_time_and_name_wo_zfill):
+def create_main_schedule_content(max_time_lenght, max_str_lenght, sorted_time_list, dict_time_and_name):
     """Create main content of schedule."""
     main_schedule_content = []
     for time in sorted_time_list:
-        main_schedule_content.append(f"""|{(max_time_lenght - len(time) - 1) * " "}{time} | {", ".join(dict_time_and_name_wo_zfill[time])}{(
-        max_str_lenght - ((len(", ".join(dict_time_and_name_wo_zfill[time]))) + 1)) * " "}|""")
+        main_schedule_content.append(f"""|{(max_time_lenght - len(time) - 1) * " "}{time} | {", ".join(dict_time_and_name[time])}{(
+        max_str_lenght - ((len(", ".join(dict_time_and_name[time]))) + 1)) * " "}|""")
     return main_schedule_content
 
 
