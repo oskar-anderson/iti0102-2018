6a59afc2e2c26b613c1c98afc0a2e3ad2b7cffd6
kaande <kaande@ttu.ee>
2018-11-09 23:16:48 +0200

    recursion

--- wat/wat.py
+++ wat/wat.py
@@ -1,26 +1,15 @@
 """wat."""
 
-first_special_ans3 = [6, 4]
-first_special_ans4 = [4, 6]
-first_special_ans5 = [4, 6]
+first_special_ans = [4, 6]
 first_general_ans = [0, 2, 8, 11, 12, 14, 16, 18]
 
 
 def first(n: int):
     """First."""
     if n in range(3, 6):
-        if n == 3:
-            ans = first_special_ans3[0]
-            first_special_ans3.remove(ans)
-            return ans
-        elif n == 4:
-            ans = first_special_ans4[0]
-            first_special_ans4.remove(ans)
-            return ans
-        else:
-            ans = first_special_ans5[0]
-            first_special_ans5.remove(ans)
-            return ans
+        ans = first_special_ans[0]
+        first_special_ans.remove(ans)
+        return ans
     else:
         ans = first_general_ans[0]
         first_general_ans.remove(ans)
